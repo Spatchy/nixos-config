6ccb46c7e6315d2e82071c5fbc125355f5577986
Spatchy <spatchy@spatchy.net>
2026-08-25 02:26:50 +0100

    Add config for Stork

--- modules/hosts/Falcon/configuration.nix
+++ modules/hosts/Falcon/configuration.nix
@@ -22,18 +22,6 @@
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-    };
-
     # Enable GoXLR utils
     services.goxlr-utility.enable = true;
 
---
+++ modules/hosts/Stork/configuration.nix
@@ -0,0 +1,76 @@
+{self, inputs, ...}: {
+
+  flake.nixosModules.StorkConfig = { pkgs, lib, ... }: {
+
+    imports = [
+      self.nixosModules.StorkHardware
+      self.nixosModules.fonts
+      self.nixosModules.common-settings
+      self.nixosModules.virtualisation
+      self.nixosModules.desktop-apps
+    ];    
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "Stork"; # Define your hostname.
+    networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Enable the KDE Plasma Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+
+    # Enable ZSH globally to enable nixpkgs vendor completions
+    programs.zsh.enable = true;
+    environment.pathsToLink = [ "/share/zsh" ];
+
+    # Set the default shell for all users
+    users.defaultUserShell = pkgs.zsh;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.james = {
+      isNormalUser = true;
+      description = "James";
+      extraGroups = [ "networkmanager" "wheel" "libvirtd" "adbusers" "dialout"];
+      packages = with pkgs; [ ];
+    };
+
+    # Enable xone gamepad driver
+    hardware.xone.enable = true;
+
+    programs.kdeconnect.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      vesktop
+      moonlight-qt
+      zapzap
+      vscodium
+      inkscape
+      feishin
+      librewolf
+      orca-slicer
+      rpi-imager
+      # Shell utils
+      pv
+      unrar
+      minicom
+      android-tools
+      yt-dlp
+      zip
+      # KDE specific packages
+      kdePackages.partitionmanager
+      # Libs
+      icu
+    ];
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.05"; # Did you read the comment?
+
+  };
+}
